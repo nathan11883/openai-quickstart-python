7435c2d760ee0bbf6f47918c8d0e3eda479e234a
Nathan Yao <nathandyao@gmail.com>
2022-08-16 21:57:50 -0700

    first working version

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+# http://127.0.0.1:5000/
 import os
+from tkinter.messagebox import QUESTION
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
@@ -10,11 +12,11 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        QUESTION = request.form["question"]
         response = openai.Completion.create(
             model="text-davinci-002",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            prompt=generate_prompt(QUESTION),
+            temperature=0,max_tokens=600,
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
@@ -22,14 +24,7 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+def generate_prompt(QUESTION):
+    return """{}""".format(
+        QUESTION.capitalize()
     )
